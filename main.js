54148ffa8f05b664d0aed39631f5b8976dafcdb9
bee-thedev <beenyshsaeed@gmail.com>
2022-10-31 23:23:17 +0500

    Update main.js

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ document.querySelector('button').addEventListener('click', apiRequest)
 async function apiRequest(){
     const rapperName = document.querySelector('input').value
     try{
-        const response = await fetch(`https://easy-erin-trout-sock.cyclic.app/api/${playerName}`)
+        const response = await fetch(`https://easy-erin-trout-sock.cyclic.app/api/${topper}`)
         const data = await response.json()
 
         console.log(data)
