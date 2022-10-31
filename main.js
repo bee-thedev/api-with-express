5f87a3d11e4a9b54106fe5623e5112bab5cd65f5
bee-thedev <beenyshsaeed@gmail.com>
2022-10-31 23:22:02 +0500

    Update main.js

---
+++ main.js
@@ -0,0 +1,14 @@
+document.querySelector('button').addEventListener('click', apiRequest)
+
+async function apiRequest(){
+    const rapperName = document.querySelector('input').value
+    try{
+        const response = await fetch(`https://easy-erin-trout-sock.cyclic.app/api/${playerName}`)
+        const data = await response.json()
+
+        console.log(data)
+        document.querySelector('h2').innerText = data.first
+    }catch(error){
+        console.log(error)
+    }
+}
