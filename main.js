2d6a517841e36905568fbecd46eca802a33664d9
bee-thedev <beenyshsaeed@gmail.com>
2022-11-14 20:40:05 +0500

    API successfully integrated into my project using Cyclic Hosting

--- main.js
+++ main.js
@@ -3,11 +3,14 @@ document.querySelector('button').addEventListener('click', apiRequest)
 async function apiRequest(){
     const topper = document.querySelector('input').value
     try{
-        const response = await fetch(`/api/${topper}`)
+        const response = await fetch(`https://easy-erin-trout-sock.cyclic.app/api/${topper}`)
         const data = await response.json()
 
         console.log(data)
         document.querySelector('h2').innerText = data.first
+        document.querySelector('h3').innerText = data.last
+        document.querySelector('h4').innerText = data.points
+
     }catch(error){
         console.log(error)
     }
